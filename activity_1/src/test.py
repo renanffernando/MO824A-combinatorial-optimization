24317245068214b9dd97747249ed4fbe08e5989c
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-03-31 17:13:44 -0300

    feat(activity_1): add ProblemSizeCounter

--- activity_1/src/test.py
+++ activity_1/src/test.py
@@ -1,6 +1,16 @@
 from CompanyProblemSolver import CompanyProblemSolver
+from ProblemSizeCounter import ProblemSizeCounter
 
 if __name__ == "__main__":
     J = 100
-    problem = CompanyProblemSolver(J)
-    print(problem)
+    solver = CompanyProblemSolver(J)
+    print(solver)
+
+    counter = ProblemSizeCounter(
+        solver.problem.J,
+        solver.problem.F,
+        solver.problem.L,
+        solver.problem.M,
+        solver.problem.P
+    )
+    print(counter)
