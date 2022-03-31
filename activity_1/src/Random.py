01d1a7930c1893f62097ee92cd364c05c04d7e73
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-03-31 16:39:07 -0300

    fix: provide a random seed to numpy so it always generates the same output

--- activity_1/src/Random.py
+++ activity_1/src/Random.py
@@ -1,14 +1,18 @@
 import numpy as np
 
 class Random:
+
+    randomSeed = 0.2315553585
+    randomGenerator = np.random.RandomState(randomSeed)
+
     @staticmethod
     def randint (low, high):
         """
         Genrerate an random int in the interval [low, high]
         """
-        return np.random.randint(low, high+1)
+        return Random.randomGenerator.randint(low, high+1)
 
     @staticmethod
     def randArray (*size, low, high):
         assert low < high
-        return (high - low) * np.random.rand(*size) + low
+        return (high - low) * Random.randomGenerator.rand(*size) + low
