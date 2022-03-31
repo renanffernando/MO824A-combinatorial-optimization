d811ca4057d7316c3014b750579f231b8fade593
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-03-30 23:44:07 -0300

    fix(activity_1): improve script for running the problem several times

--- activity_1/src/timeExecution.py
+++ activity_1/src/timeExecution.py
@@ -1,26 +1,44 @@
 import timeit
+from CompanyProblemSolver import CompanyProblemSolver
 
-def getExecutionTime(J):
-    repetitions = 3
-    totalExecutionTime = timeit.timeit(
-        stmt = f"CompanyProblemSolver({J})",
-        setup = "from CompanyProblemSolver import CompanyProblemSolver",
-        number = repetitions
-    )
-    return totalExecutionTime / repetitions
+class TimedSolution:
+    def __init__(self, J):
+        problem = CompanyProblemSolver(J)
+        self.numberOfVariables = problem.numberOfVariables
+        self.numberOfConstraints = problem.numberOfConstraints
+        self.cost = problem.cost
+        self.executionTime = TimedSolution.getExecutionTime(J)
 
-problemSizes = range(100, 1000+1, 100)
+    @staticmethod
+    def getExecutionTime(J):
+        repetitions = 3
+        totalExecutionTime = timeit.timeit(
+            stmt = f"CompanyProblemSolver({J})",
+            setup = "from CompanyProblemSolver import CompanyProblemSolver",
+            number = repetitions
+        )
+        return totalExecutionTime / repetitions
 
-executionTimes = {}
-for J in problemSizes:
-    executionTimes[J] = getExecutionTime(J)
+if __name__ == "__main__":
+    problemSizes = range(100, 1000+1, 100)
 
-print("\n\n")
-print("Execution Times (J x time [s])")
-print(executionTimes)
-print("\n\n")
+    solutionOutput = []
+    for J in problemSizes:
+        solution = TimedSolution(J)
+        solutionOutput.append({
+            "problem size (J)"
+            "number of variables": solution.numberOfVariables,
+            "number of constraints": solution.numberOfConstraints,
+            "cost": solution.cost,
+            "execution time [s]": solution.executionTime
+        })
 
-print("\n\n")
-print("Execution times as a reproducible dict")
-print(executionTimes.__repr__())
-print("\n\n")
+    print("\n\n")
+    print("Execution Times (J x time [s])")
+    print(solutionOutput)
+    print("\n\n")
+
+    print("\n\n")
+    print("Execution times as a reproducible dict")
+    print(solutionOutput.__repr__())
+    print("\n\n")
