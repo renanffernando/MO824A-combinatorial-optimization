836a5cf0f3f9fb29bac6e0d98572b02e77a137ea
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-03-30 21:28:24 -0300

    feat(activity_1): add problem constants

---
+++ activity_1/src/Constants.py
@@ -0,0 +1,28 @@
+class Constants:
+    class L:
+        low = 5
+        high = 10
+    class M:
+        low = 5
+        high = 10
+    class P:
+        low = 5
+        high = 10
+    class D:
+        low = 10
+        high = 20
+    class r:
+        low = 1
+        high = 5
+    class R:
+        low = 800
+        high = 1000
+    class C:
+        low = 80
+        high = 100
+    class p:
+        low = 10
+        high = 100
+    class t:
+        low = 10
+        high = 20
