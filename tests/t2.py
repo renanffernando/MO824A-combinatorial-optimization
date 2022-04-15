69aa20064a24e5cab7678e96ad299c09d7611119
Renan <renanfernandofranco@gmail.com>
2022-04-15 20:37:33 -0300

    Convert tests to logs

---
+++ tests/t2.py
@@ -0,0 +1,44 @@
+from math import ceil
+import sys
+import os
+
+nT = 250
+
+ns = [100, 150, 200, 250]
+kMult = [0, 0.5, 1]
+
+outdir = sys.argv[1]
+obj = []
+bounds = []
+gaps = []
+times = []
+	
+for n in ns:
+	for kM in kMult:
+		k = int(kM * n)
+		name = "N%dK%d.ktsp" % (n, k)
+		with open(outdir + "/" + name, "r") as instFile:
+			while True:
+				line = instFile.readline().split()
+				if len(line) < 2:
+					continue
+				if line[0] == "Explored":
+					times.append("%.1f" % float(line[7]))
+				if (line[0] == "Optimal"  and line[1] == "solution") or (line[0] == "Time" and line[1] == "limit"):
+					line = instFile.readline().replace(",", "")
+					line = line.replace("%", "")
+					line = line.split()
+					assert(line[0] == "Best" and line[1] == "objective")
+					assert(line[3] == "best" and line[4] == "bound")
+					assert(line[6] == "gap")
+					bestObj = int(float(line[2]))
+					bestBound = int(float(line[5]))
+					gap = "%.2f" % float(line[7])
+					obj.append(bestObj)
+					bounds.append(bestBound)
+					gaps.append(gap)
+					break
+
+for i in range(len(obj)):
+    print(obj[i], bounds[i], gaps[i], times[i])
+    
