9dfc8d97e0291f2263db32fb37a329f0aeecb529
Renan <renanfernandofranco@gmail.com>
2022-04-28 17:34:44 -0300

    feat(activity_3): add apply and update lambda functions

--- activity_3/include/heuristic.hpp
+++ activity_3/include/heuristic.hpp
@@ -1,24 +1,10 @@
 #ifndef HEURISTIC_H
 #define HEURISTIC_H
 
-#include <bits/stdc++.h>
-
-using namespace std;
-using vi = vector<int>;
-using vvi = vector<vi>;
-using ld = double;
-using vd = vector<ld>;
-using vvd = vector<vd>;
-using ii = pair<int, int>;
-using vii = vector<ii>;
-
-#define all(a) a.begin(), a.end()
-#define EPS 1e-4
-#define FOR(i, b) for(int i = 0; i < (b); i++)
-#define SZ(a) ((int) a.size())
+#include "util.hpp"
 
 namespace Heuristic{
-    void IncreaseIntersection(vi& tour1, vi& tour2, vvd& d1, vvd& d2);
+  void IncreaseIntersection(vi& tour1, vi& tour2, vvd& d1, vvd& d2);
 }
 
 #endif
---
+++ activity_3/src/lagrangian.cpp
@@ -0,0 +1,34 @@
+#include "lagrangian.hpp"
+
+vvd apllyLambdaToCost(const vd& lambda, const vvd& cost) {
+  int n = SZ(cost);
+  assert(SZ(lambda) == n * n);
+  vvd ret = cost;
+  FOR(i, n)
+    FOR(j, n)
+      ret[i][j] = cost[i][j] - lambda[i * n + j * n];
+  return cost;
+}
+
+void updateLambda(const vvd& x1, const vvd& x2, const vvd& z, vd& lambda1, vd& lambda2, ld lb, ld ub){
+  int n = SZ(x1);
+  vvd grad1(n, vd(n)), grad2(n, vd(n));
+  FOR(i, n)
+    FOR(j, n){
+      grad1[i][j] = z[i][j] - x1[i][j];
+      grad2[i][j] = z[i][j] - x2[i][j];
+    }
+  
+  ld norm = 0;
+  FOR(i, n)
+    FOR(j, n)
+      norm += grad1[i][j] * grad1[i][j] + grad2[i][j] * grad2[i][j];
+
+  ld alpha = (sqrt(5) - 1) / 2 * (ub - lb) / norm;
+
+  FOR(i, n)
+    FOR(j, n){
+      lambda1[i * n + j] = max(0.0, lambda1[i * n + j] + alpha * grad1[i][j]);
+      lambda2[i * n + j] = max(0.0, lambda2[i * n + j] + alpha * grad2[i][j]);
+    }
+}
