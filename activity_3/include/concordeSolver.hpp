1ae6337b1bb6d5757f0709ee86beb9885f04e58f
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-04-28 11:05:17 -0300

    refactor(activity_3): create 'concordeSolver' out of 'concorde_example' code

---
+++ activity_3/include/concordeSolver.hpp
@@ -0,0 +1,8 @@
+#ifndef CONCORDE_SOLVER_HPP_
+#define CONCORDE_SOLVER_HPP_
+
+#include <vector>
+
+int solve_tsp_concorde (const std::vector<std::vector<int>>& distance, std::vector<int>& tour);
+
+#endif
--- activity_3/src/concorde_example.cpp
+++ activity_3/src/concorde_example.cpp
@@ -1,50 +1,7 @@
-extern "C" {
-  #include <concorde.h>
-}
+#include "concordeSolver.hpp"
 
 #include <iostream>
 #include <vector>
-#include <numeric>
-
-int solve_tsp_concorde (const std::vector<std::vector<int>>& distance, std::vector<int>& tour)
-{
-  int rval = 0;
-  const int ncount = distance.size();
-  if (static_cast<int>(tour.size()) != ncount) {
-    tour.resize(ncount);
-  }
-  if (ncount < 5) {
-    std::cerr << "Less than 5 nodes.";
-    return 1;
-  }
-
-  int seed = rand();
-  CCrandstate rstate;
-  CCutil_sprand(seed, &rstate);
-  const int ecount = (ncount * (ncount - 1)) / 2;
-  std::vector<int> elist(ecount * 2);
-  std::vector<int> elen(ecount);
-  int edge = 0;
-  int edgeWeight = 0;
-  for (int i = 0; i < ncount; i++) {
-    for (int j = i + 1; j < ncount; j++) {
-      elist[edge++] = i;
-      elist[edge++] = j;
-      elen[edgeWeight++] = distance[i][j];
-    }
-  }
-  char *name = CCtsp_problabel(" ");
-  CCdatagroup dat;
-  CCutil_init_datagroup (&dat);
-  rval = CCutil_graph2dat_matrix (ncount, ecount, elist.data(), elen.data(), 1, &dat);
-
-  double  optval, *timebound = nullptr;
-  int success, foundtour, hit_timebound = 0;
-  rval = CCtsp_solve_dat (ncount, &dat, nullptr, tour.data(), NULL, &optval, &success,
-    &foundtour, name, timebound, &hit_timebound, 1, &rstate);
-
-  return rval;
-}
 
 int main()
 {
