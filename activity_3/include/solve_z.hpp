a4160f9a688eb929eda429555710f75934a35e7d
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-04-28 21:20:46 -0300

    feat(activity_3): add 'computeCost' to 'solve_z'

--- activity_3/include/solve_z.hpp
+++ activity_3/include/solve_z.hpp
@@ -4,8 +4,15 @@
 #include <vector>
 
 std::vector<int> solve_z(
-    const std::vector<double> & cost,
+    const std::vector<double> & cost0,
+    const std::vector<double> & cost1,
     const int sigma
+);
+
+double computeCost (
+    const std::vector<double> & cost0,
+    const std::vector<double> & cost1,
+    const std::vector<int> & z
 );
 
 #endif
--- activity_3/src/solve_z.cpp
+++ activity_3/src/solve_z.cpp
@@ -4,12 +4,43 @@
 #include <utility>
 #include <algorithm>
 #include <iostream>
+#include <cassert>
+
+std::vector<int> sumCosts(
+    const std::vector<double> & cost0,
+    const std::vector<double> & cost1
+) {
+    assert(cost0.size() == cost1.size());
+    std::vector<int> sum (cost0.size());
+    for (std::size_t i = 0; i < cost0.size(); ++i)
+    {
+        sum[i] = cost0[i] + cost1[i];
+    }
+    return sum;
+}
+
+double computeCost (
+    const std::vector<double> & cost0,
+    const std::vector<double> & cost1,
+    const std::vector<int> & z
+) {
+    assert(cost0.size() == cost1.size());
+    assert(cost0.size() == z.size());
+    double acc = 0;
+    for (std::size_t i = 0; i < z.size() ; ++i)
+    {
+        acc += (cost0[i] + cost1[i]) * z[i];
+    }
+    return acc;
+}
 
 std::vector<int> solve_z(
-    const std::vector<double> & cost,
+    const std::vector<double> & cost0,
+    const std::vector<double> & cost1,
     const int sigma
 ) {
     std::vector<std::pair<double,std::size_t>> value_index_vector;
+    const auto cost = sumCosts(cost0, cost1);
     value_index_vector.reserve(cost.size());
     for (std::size_t i = 0; i < cost.size() ; ++i)
     {
