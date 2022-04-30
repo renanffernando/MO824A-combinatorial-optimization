fb4a4f2096c153b4d5acea2ad51fa32c7b876ff5
Renan <renanfernandofranco@gmail.com>
2022-04-30 18:42:00 -0300

    feat(activity_3): add script to plot the results

--- activity_3/results_old/extract_data.py
+++ activity_3/results_old/extract_data.py
@@ -1,5 +1,6 @@
 import os
 import re
+import matplotlib.pyplot as plt
 
 def find_all(l, regex):
     out = []
@@ -9,12 +10,12 @@ def find_all(l, regex):
             out.append(found)
     return list(map(lambda s: s.group(1), out))
 
-
-
 all_files = os.listdir('./')
 all_files = list(filter(lambda file_name: file_name.endswith(".log"), all_files))
+all_files = ["./" + file for file in all_files]
+print(all_files)
 
-for file_name in ['./N100K50.log']:
+for file_name in all_files:
     with open(file_name) as file:
         lines = list(map(lambda x: str(x), file.readlines()))
         print(lines[:10])
@@ -23,7 +24,23 @@ for file_name in ['./N100K50.log']:
         learning_rates = find_all(lines, '^learning rate: (\d.?\d*)$')
         processing_times = find_all(lines, '^Processing time: (\d+) \[s\]$')
 
+        lower_bounds = list(map(float, lower_bounds))
+        upper_bounds = list(map(float, upper_bounds))
+        xdata = [_ for _ in range(len(upper_bounds))]
+        plt.plot(xdata, lower_bounds, color='tab:red', label = "Lower Bound")
+        plt.plot(xdata, upper_bounds, color='tab:blue', label = "Upper Bound")
+        plt.xlabel('Iterations')
+        plt.ylabel('Value')
+        plt.grid(color='black', linestyle='-.', linewidth=0.5)
+        plt.legend()
+        name = file_name.replace(".log", "").replace("./", "")
+        plt.title("Instance " + name)
+        plt.savefig(name + ".png")
+        plt.close()
+
+'''
 print(upper_bounds)
 print(lower_bounds)
 print(learning_rates)
 print(processing_times)
+'''
