002c9198ae8a18383256affdd63e709ceb27a817
renanfernandofranco <renanfernandofranco@gmail.com>
2022-06-17 04:44:32 -0300

    add tests output

--- activity6/src/maxQbf_c++/main.cpp
+++ activity6/src/maxQbf_c++/main.cpp
@@ -1,23 +1,25 @@
 #include "GA.hpp"
 
-int main(){
+int main()
+{
   cin.tie(0)->sync_with_stdio(0);
 
-  Instance* instance = Instance::readInstance();
+  Instance *instance = Instance::readInstance();
   const int timeLimit = 60000 * 30;
-  const int maxGenerations = 20000;
+  const int maxGenerations = 10000;
   vi popSizes = {100};
 
-  for(int popSize : popSizes){
+  for (int popSize : popSizes)
+  {
     GA ga(instance, popSize, timeLimit, maxGenerations);
-    Solution* sol = ga.run();
+    Solution *sol = ga.run();
 
     assert(sol->value == Solution::computeValue(sol));
     assert(sol->getWeight() <= instance->W);
 
     cout << setprecision(3) << fixed;
     cout << "PopSize: : " << popSize << " - Cost: " << sol->getCost();
-    cout << " - Weight: " << sol->getWeight() << " - Time: " << sol->elapsedTime/1000.0;
+    cout << " - Weight: " << sol->getWeight() << " - Time: " << sol->elapsedTime / 1000.0;
     cout << " - generations: " << sol->generations;
     cout << endl;
     delete sol;
--- activity6/src/maxQbf_c++/runall.sh
+++ activity6/src/maxQbf_c++/runall.sh
@@ -1,5 +1,5 @@
-rm -r out
-mkdir out
+rm -r output
+mkdir output
 make
-for i in ../../instances/kqbf/kqbf*; do INST=$(echo $i | rev | cut --delimiter="/" -f 1 | rev); echo "Running $INST"; ./maxQbf_c++ < ../../instances/kqbf/$INST | tee -a out/$INST.out > /dev/null; done 
+for i in ../../instances/kqbf/kqbf*; do INST=$(echo $i | rev | cut --delimiter="/" -f 1 | rev); echo "Running $INST"; ./maxQbf_c++ < ../../instances/kqbf/$INST | tee -a output/$INST.ans > /dev/null; done 
 rm maxQbf
